c5720b9a3cbe39bdd01e91b101a1f61c43335a0e
James M. Greene <jamesmgreene@github.com>
2020-08-18 16:14:17 -0500

    Support version comparsions both with and without plans for both expected and given values

--- liquid.js
+++ liquid.js
@@ -13,16 +13,48 @@ engine.registerFileSystem(
 
 Liquid.Condition.operators.ver_gt = (cond, left, right) => {
   if (!includesNumber(left)) return false
-  return semver.gt(semver.coerce(left), semver.coerce(right))
+  if (!includesNumber(right)) return false
+
+  const [leftPlan, leftRelease] = splitVersion(left)
+  const [rightPlan, rightRelease] = splitVersion(right)
+
+  if (leftPlan !== rightPlan) return false
+
+  return semver.gt(semver.coerce(leftRelease), semver.coerce(rightRelease))
 }
 
 Liquid.Condition.operators.ver_lt = (cond, left, right) => {
   if (!includesNumber(left)) return false
-  return semver.lt(semver.coerce(left), semver.coerce(right))
+  if (!includesNumber(right)) return false
+
+  const [leftPlan, leftRelease] = splitVersion(left)
+  const [rightPlan, rightRelease] = splitVersion(right)
+
+  if (leftPlan !== rightPlan) return false
+
+  return semver.lt(semver.coerce(leftRelease), semver.coerce(rightRelease))
 }
 
 module.exports = engine
 
 function includesNumber (input) {
   return input && input.match(/\d+/)
+}
+
+function splitVersion (version) {
+  // The default plan when working with versions is "enterprise-server".
+  // Default to that value here to support backward compatibility from before
+  // plans were explicitly included.
+  let plan = 'enterprise-server'
+  let release
+
+  const lastIndex = version.lastIndexOf('@')
+  if (lastIndex === -1) {
+    release = version
+  } else {
+    plan = version.slice(0, lastIndex)
+    release = version.slice(lastIndex + 1)
+  }
+
+  return [plan, release]
 }
