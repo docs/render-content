0a6e75605bb79c112fa88a87d399634cdbe2338b
James M. Greene <JamesMGreene@github.com>
2020-08-20 07:13:18 -0500

    fix: test all the things! (#30)
    * Consolidate ver_gt/ver_lt tests into one file and add a lot more
    
    * Support version comparsions both with and without plans for both expected and given values
    
    * Add comment explaining version format
    
    Co-authored-by: Sarah Schneider <sarahs@users.noreply.github.com>
    
    Co-authored-by: Sarah Schneider <sarahs@github.com>
    Co-authored-by: Sarah Schneider <sarahs@users.noreply.github.com>

--- liquid.js
+++ liquid.js
@@ -13,16 +13,51 @@ engine.registerFileSystem(
 
 Liquid.Condition.operators.ver_gt = (cond, left, right) => {
   if (!matchesVersionString(left)) return false
-  return semver.gt(semver.coerce(left), semver.coerce(right))
+  if (!matchesVersionString(right)) return false
+
+  const [leftPlan, leftRelease] = splitVersion(left)
+  const [rightPlan, rightRelease] = splitVersion(right)
+
+  if (leftPlan !== rightPlan) return false
+
+  return semver.gt(semver.coerce(leftRelease), semver.coerce(rightRelease))
 }
 
 Liquid.Condition.operators.ver_lt = (cond, left, right) => {
   if (!matchesVersionString(left)) return false
-  return semver.lt(semver.coerce(left), semver.coerce(right))
+  if (!matchesVersionString(right)) return false
+
+  const [leftPlan, leftRelease] = splitVersion(left)
+  const [rightPlan, rightRelease] = splitVersion(right)
+
+  if (leftPlan !== rightPlan) return false
+
+  return semver.lt(semver.coerce(leftRelease), semver.coerce(rightRelease))
 }
 
 module.exports = engine
 
 function matchesVersionString (input) {
   return input && input.match(/(?:^[a-z-]+@)?([\d.]+)/)
+}
+// Support new version formats where version = plan@release
+// e.g., enterprise-server@2.21, where enterprise-server is the plan and 2.21 is the release
+// e.g., free-pro-team@latest, where free-pro-team is the plan and latest is the release
+// in addition to legacy formats where the version passed is simply 2.21
+function splitVersion (version) {
+  // The default plan when working with versions is "enterprise-server".
+  // Default to that value here to support backward compatibility from before
+  // plans were explicitly included.
+  let plan = 'enterprise-server'
+  let release
+
+  const lastIndex = version.lastIndexOf('@')
+  if (lastIndex === -1) {
+    release = version
+  } else {
+    plan = version.slice(0, lastIndex)
+    release = version.slice(lastIndex + 1)
+  }
+
+  return [plan, release]
 }
