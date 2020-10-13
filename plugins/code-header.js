397939160a8382dc2204e072258b0ad807a1fc73
Jason Etcovitch <jasonetco@github.com>
2020-10-13 15:11:26 -0400

    Add comments

--- plugins/code-header.js
+++ plugins/code-header.js
@@ -73,24 +73,32 @@ const LANGUAGE_MAP = {
   jsx: 'JSX'
 }
 
+const COPY_REGEX = /\{:copy\}$/
+
 /**
  * Adds a bar above code blocks that shows the language and a copy button
  */
 module.exports = function addCodeHeader (node) {
-  const hasCopy = node.lang && node.lang.endsWith('{:copy}')
+  // Check if the language matches `lang{:copy}`
+  const hasCopy = node.lang && COPY_REGEX.test(node.lang)
 
-  if (hasCopy && node.lang) {
-    node.lang = node.lang.replace(/\{:copy\}$/, '')
+  if (hasCopy) {
+    // js{:copy} => js
+    node.lang = node.lang.replace(COPY_REGEX, '')
   } else {
+    // It doesn't have the copy annotation, so don't add the header
     return
   }
 
+  // Display the language using the above map of `{ [shortCode]: language }`
   const language = LANGUAGE_MAP[node.lang] || node.lang || 'Code'
 
   const btnIconHtml = octicons.clippy.toSVG()
   const btnIconAst = parse5.parse(String(btnIconHtml))
   const btnIcon = fromParse5(btnIconAst, btnIconHtml)
 
+  // Need to create the header using Markdown AST utilities, to fit
+  // into the Unified processor ecosystem.
   const header = h(
     'header',
     {
